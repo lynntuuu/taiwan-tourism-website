43f2dea892abb99313a45063ad5b0889897c9e9e
lynntuuu <lynntuuu@gmail.com>
2023-06-19 11:27:48 +0800

    chore(project): create project

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      imports: ["vue", "vue-router", "vue-i18n"],
+      dts: "src/auto-imports.js",
+    }),
+    Components({
+      // 從 `./src/components/` 路徑查找
+      extensions: ["vue"],
+      include: [/\.vue$/, /\.vue\?vue/],
+      dts: "src/auto-components.js",
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
